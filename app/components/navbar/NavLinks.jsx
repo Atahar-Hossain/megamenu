8fb013e41ed638748d4bd898302acdea6a5a4cc6
M A H Mahadi <65162473+Atahar-Hossain@users.noreply.github.com>
2024-04-16 18:26:09 +0600

    updated

--- app/components/navbar/NavLinks.jsx
+++ app/components/navbar/NavLinks.jsx
@@ -1,13 +1,52 @@
 import React from 'react'
-
+import Link from 'next/link'
 const NavLinks = () => {
-    const links=[{name:"Men", id:1},{name:"Women", id:2}, {name: "Kid's", id:3}]
+    const links = [
+        {
+            name: "Men", id: 1, submenu: true, sublinks: [
+                {
+                    Head: "TopWear",
+                    sublink: [
+                        {name:"T-Shirt", link:"/", id:1.1},
+                        {name:"Casual Shirts", link:"/", id:1.2},
+                        {name:"Formal Shirts", link:"/", id:1.3},
+                        {name:"Regular Fit", link:"/", id:1.4},
+                    ]
+            }
+        ] },
+        { name: "Women", id: 2 },
+        { name: "Kid's", id: 3 }]
   return (
       <>
           {links.map((link) => (
               <div key={link.id}>
                   <div className='px-3 text-left md:cursor-pointer'>
-                      <h2 className="py-7">{ link.name}</h2>
+                      <h2 className="py-7">{link.name}</h2>
+                      {link.submenu && (
+                          <div>
+                              <div className='absolute top-20'>
+                                  <div>
+                                      <div className='py-3'>
+                                          <div className='w-4 h-4 mt-1 left-3 rotate-45 absolute bg-white'></div>
+                                      </div>
+                                  </div>
+                                  <div className='bg-white p-3.5'>
+                                      {link.sublinks.map((mysublinks) => (
+                                          <div key={link.id}>
+                                              <h2 className='text-[22px] font-bold uppercase'>{ mysublinks.Head}</h2>
+                                              {mysublinks.sublink.map(slink => (
+                                                  <li key={link.id} className='text-md text-gray-600 my-2.5'>
+                                                      <Link href={slink.link}
+                                                      className='hover:text-purple-400'
+                                                      >{slink.name}</Link>
+                                                  </li>
+                                              ))}
+                                          </div>
+                                      ))}
+                                  </div>
+                              </div>
+                          </div>
+                      )}
                   </div>
             </div>
         ))}
--- app/components/navbar/navbar.jsx
+++ app/components/navbar/navbar.jsx
@@ -1,19 +1,22 @@
-import React from 'react'
+"use client"
+import React, { useState } from 'react'
 import Link from 'next/link'
 import NavLinks from './NavLinks'
 import Button from '../Button'
 import { TfiMenuAlt } from "react-icons/tfi";
+import { FaWindowClose } from "react-icons/fa";
 
 
 
 const navbar = () => {
+  const[open, setOpen]= useState(false)
   return (
     <nav className='bg-white'>
       <div className='flex items-center font-bold justify-around font-[quicksand]'>
-        <div className='z-50 md:w-auto w-full p-5'>
+        <div className='z-50 md:w-auto w-full p-5 flex justify-between cursor-pointer'>
           <p>Logo Here</p>
-          <div className="text-3xl">
-            <TfiMenuAlt />
+          <div className="text-3xl md:hidden" onClick={()=>setOpen(!open)}>
+            {open?<FaWindowClose/>:<TfiMenuAlt/>}
           </div>
         </div>
         
@@ -28,7 +31,7 @@ const navbar = () => {
         </div>
         {/* mobile menu */}
         <ul className={`
-        md:hidden bg-white absolute w-full h-full bottom-0 py-24 pl-5
+        md:hidden bg-white absolute w-full h-full bottom-0 py-24 pl-5 duration-500 ${open?'left-0':'bottom-[-100%]'}
         `}>
           <li>
             <Link href={"/"} className="py-7 px-3 inline-block">Home</Link>
